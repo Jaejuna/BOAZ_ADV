0f855e4368516b7adbfbc737438c8ef34c256d72
이찬 <cksdlakstp12@naver.com>
2023-12-15 23:35:09 +0900

    update

--- to_make_map.py
+++ to_make_map.py
@@ -1,36 +1,13 @@
 import utils.setup_path as setup_path
 import airsim
 
+from math import pi
 from datetime import datetime
 import numpy as np
 import cv2
 
 from tools.vision import *
-
-def get_transformed_lidar_pc(client):
-   # raw lidar data to open3d PointCloud
-   lidar_data = client.getLidarData()
-   lidar_points = np.array([lidar_data.point_cloud[i:i+3] for i in range(0, len(lidar_data.point_cloud), 3)])
-   pcd = o3d.geometry.PointCloud()
-   pcd.points = o3d.utility.Vector3dVector(lidar_points)
-
-   # calc rotation matrix
-   orientation = lidar_data.pose.orientation
-   q0, q1, q2, q3 = orientation.w_val, orientation.x_val, orientation.y_val, orientation.z_val
-   rotation_matrix = np.array(([1-2*(q2*q2+q3*q3),2*(q1*q2-q3*q0),2*(q1*q3+q2*q0)],
-                               [2*(q1*q2+q3*q0),1-2*(q1*q1+q3*q3),2*(q2*q3-q1*q0)],
-                               [2*(q1*q3-q2*q0),2*(q2*q3+q1*q0),1-2*(q1*q1+q2*q2)]))
-   
-   # calc translation vector
-   position = lidar_data.pose.position
-   x_val, y_val, z_val = position.x_val, position.y_val, position.z_val
-   translation_vector = np.array([x_val, y_val, z_val])
-
-   # apply transform
-   transformation_matrix = np.eye(4)
-   transformation_matrix[:3, :3] = rotation_matrix
-   transformation_matrix[:3, 3] = translation_vector
-   return pcd.transform(transformation_matrix), list(map(str, [q0, q1, q2, q3, x_val, y_val, z_val]))
+from tools.train_utils import *
 
 # connect to the AirSim simulator
 client = airsim.MultirotorClient()
@@ -40,34 +17,52 @@ client.enableApiControl(True)
 client.armDisarm(True)
 client.takeoffAsync().join()
 
+
 # global_pcd = o3d.geometry.PointCloud() # 새롭게 맵을 만들때 사용
 global_pcd = o3d.io.read_point_cloud("./results/map_created_by_lidar.ply") # 기존에 만들던 맵을 계속해서 사용할 때 사용
 
 while 1:
-   input_data = input("input data : ")
-   if input_data == "break": break
-   elif input_data == "reset": 
-      client.reset()
-   try:
-      input_data = list(map(float, input_data.split(" ")))
-   except:
-      print("something wrong...")
-      continue
+   input_data = int(input("input data : "))
 
-   if len(input_data) == 3:
+   if input_data == 0:
       # client.moveToPositionAsync(*input_data, 5).join()
-      client.moveByVelocityAsync(*input_data, 1).join()
-   elif len(input_data) == 1:
-      client.rotateToYawAsync(*input_data).join()
+      forward = calcForwardDirection(client)
+      client.moveByVelocityAsync(*list(map(lambda x:x*2, forward)), 2).join()
+   elif input_data == 1 or input_data == 2:
+      degree = calcDegree(client, input_data)
+      client.rotateToYawAsync(degree).join()
    else:
       print("check your input")
       continue
 
-   client.simPause(True)
-   pcd, state = get_transformed_lidar_pc(client)
-   global_pcd = global_pcd + pcd
-   global_pcd = global_pcd.voxel_down_sample(voxel_size=0.05)
-   pose = client.simGetVehiclePose()
-   print(pose.position.x_val, pose.position.y_val, pose.position.z_val)
-   client.simPause(False)
-   o3d.io.write_point_cloud("./results/map_created_by_lidar.ply", global_pcd)
+# while 1:
+#    input_data = input("input data : ")
+#    if input_data == "break": break
+#    elif input_data == "reset": 
+#       client.reset()
+#    try:
+#       input_data = list(map(float, input_data.split(" ")))
+#    except:
+#       print("something wrong...")
+#       continue
+
+#    if len(input_data) == 3:
+#       # client.moveToPositionAsync(*input_data, 5).join()
+#       client.moveByVelocityAsync(*input_data, 1).join()
+#    elif len(input_data) == 1:
+#       client.rotateToYawAsync(*input_data).join()
+#    else:
+#       print("check your input")
+#       continue
+#    state = client.getMultirotorState()
+#    orientation = state.kinematics_estimated.orientation
+#    _, _, yaw = airsim.to_eularian_angles(orientation)
+
+#    client.simPause(True)
+#    pcd, state = get_transformed_lidar_pc(client)
+#    global_pcd = global_pcd + pcd
+#    global_pcd = global_pcd.voxel_down_sample(voxel_size=0.05)
+#    pose = client.simGetVehiclePose()
+#    print(pose.position.x_val, pose.position.y_val, pose.position.z_val)
+#    client.simPause(False)
+#    o3d.io.write_point_cloud("./results/map_created_by_lidar.ply", global_pcd)
