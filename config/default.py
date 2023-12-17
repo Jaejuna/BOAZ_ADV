f159280c08c12aadf659c6bef67efa45d046b33d
이찬 <cksdlakstp12@naver.com>
2023-12-18 03:04:37 +0900

    ADD : manual argument에 transform 추가

--- config/default.py
+++ config/default.py
@@ -1,5 +1,6 @@
 from easydict import EasyDict as edict
 import torch
+from torchvision import transforms
 
 drone = edict()
 drone.yaw_rate = 90
@@ -18,8 +19,16 @@ TDRS.min_decay_factor = 0
 TDRS.every_second = 10
 
 manual = edict()
-manual.txt_path = "data/name_tags.txt"
+manual.train_txt_path = "data/train_name_tags.txt"
+manual.test_txt_path = "data/test_name_tags.txt"
 manual.batch_size = 16
+
+manual.transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),  # 이미지를 수평 및 수직으로 최대 5% 이동
+    transforms.RandomResizedCrop(size=(224, 224), scale=(0.9, 1.0)),  # 원본 이미지의 80%에서 100% 범위로 랜덤 크롭
+    transforms.ToTensor(),
+])
 
 # main
 args = edict(drone=drone, TDRS=TDRS, manual=manual)
@@ -40,7 +49,7 @@ args.epsilon = 0.5
 args.min_epsilon = 0.01
 args.epsilon_decay_rate = 0.95
 
-args.batch_size = 4 # batch size
+args.batch_size = 16 # batch size
 args.mem_size = 100
 args.epochs = 5000  
 args.max_time = 300
@@ -54,4 +63,4 @@ args.eval_steps = 10
 
 args.live_visualization = False
 
-args.train_mode = "manual" # manual or auto
+args.train_mode = "auto" # manual or auto
