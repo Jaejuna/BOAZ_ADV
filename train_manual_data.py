631f8b2535c5e773d7592b17e0bab0ca7022a977
이찬 <cksdlakstp12@naver.com>
2023-12-18 03:06:41 +0900

    ADD : test 코드 추가

--- train_manual_data.py
+++ train_manual_data.py
@@ -1,23 +1,12 @@
-import utils.setup_path as setup_path
-import airsim
-
-from collections import deque
-
-import copy
-import random
 from datetime import datetime
-import multiprocessing
 
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import numpy as np
 
-from tools.test import getAccuracy
-from tools.models import MovePredictModel, RGBDepthFusionNet
 from tools.vision import *
 from tools.train_utils import * 
-from tools.live_visualization import live_visualization
 from tools.dataset import ManualDataset
 
 from config.default import args
@@ -29,39 +18,73 @@ if __name__ == '__main__':
     job_dir = os.path.join("./run", date) + "_manual"
     os.makedirs(job_dir, exist_ok=True)
 
-    device = args.device # cpu or gpu
+    logger = make_logger(job_dir)
 
-    dataset = ManualDataset(args["manual"].txt_path)
-    data_loader = DataLoader(dataset, batch_size=args["manual"].batch_size, shuffle=True)
+    best_accuracy = 0
+    device = args.device
+
+    train_dataset = ManualDataset(args["manual"].train_txt_path, args["manual"].transform)
+    test_dataset = ManualDataset(args["manual"].test_txt_path)
+    train_loader = DataLoader(train_dataset, batch_size=args["manual"].batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=args["manual"].batch_size, shuffle=False)
 
     model, _ = create_models(args)
     criterion = torch.nn.MSELoss() # 손실함수 mse 사용
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate) # 최적화 알고리즘 아담사용
 
-    for exp in data_loader:
-        rgb1, rgb2, depth1, depth2, position, action, reward, done = exp
+    for epoch in range(500):
+        print_and_logging(logger, f"\n{epoch}epoch start...")
+        for exp in train_loader:
+            rgb1, rgb2, depth1, depth2, position, action, reward, done = exp
 
-        rgb1 = rgb1.to(device).float()
-        depth1 = depth1.to(device).float()
-        position = position.to(device).float()
-        action = action.to(device).float()
-        reward = reward.to(device).float()
-        done = done.to(device).float()
+            rgb1 = rgb1.to(device).float()
+            depth1 = depth1.to(device).float()
+            position = position.to(device).float()
+            action = action.to(device).float()
+            reward = reward.to(device).float()
+            done = done.to(device).float()
 
-        Q = model(rgb1, depth1, position)
+            Q = model(rgb1, depth1, position)
 
-        softmax_batch = F.softmax(Q, dim=1).detach().cpu().numpy()
-        Q_action = [np.random.choice(np.array([0, 1, 2]), p=softmax) for softmax in softmax_batch]
-        Q_action = torch.FloatTensor(Q_action).to(device)
+            softmax_batch = F.softmax(Q, dim=1).detach().cpu().numpy()
+            Q_action = [np.random.choice(np.array([0, 1, 2]), p=softmax) for softmax in softmax_batch]
+            Q_action = torch.FloatTensor(Q_action).to(device)
 
-        Q = Q.gather(dim=1, index=Q_action.long().unsqueeze(dim=1)).squeeze()
-        Y = reward + args.gamma * ((1 - done) * action)
+            Q = Q.gather(dim=1, index=Q_action.long().unsqueeze(dim=1)).squeeze()
+            Y = reward + args.gamma * ((1 - done) * action)
 
-        loss_move = criterion(Q, Y.detach())
-        print(f"loss : {loss_move.item()}")
+            loss_move = criterion(Q, Y.detach())
+            print_and_logging(logger, f"loss : {loss_move.item()}")
 
-        optimizer.zero_grad()
-        loss_move.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            loss_move.backward()
+            optimizer.step()
 
-        torch.save(model.state_dict(), os.path.join(job_dir, 'manual_trained_model.pth')) 
+        with torch.no_grad():
+            eq_count = 0
+            for exp in test_loader:
+                rgb1, rgb2, depth1, depth2, position, action, reward, done = exp
+
+                rgb1 = rgb1.to(device).float()
+                depth1 = depth1.to(device).float()
+                position = position.to(device).float()
+                action = action.to(device).float()
+
+                Q = model(rgb1, depth1, position)
+
+                softmax_batch = F.softmax(Q, dim=1).detach().cpu().numpy()
+                Q_action = [np.random.choice(np.array([0, 1, 2]), p=softmax) for softmax in softmax_batch]
+                Q_action = torch.FloatTensor(Q_action).to(device)
+
+                eq_count += (action == Q_action).sum().item()
+
+            accuracy = eq_count / len(test_dataset)
+            print_and_logging(logger, f"test accuracy : {accuracy}")
+
+        if best_accuracy < accuracy:
+            print_and_logging(logger, "save new best model...")
+            torch.save(model.state_dict(), os.path.join(job_dir, 'best_manual_model.pth')) 
+            best_accuracy = accuracy
+
+        if epoch % 10 == 0 and epoch != 0:
+            torch.save(model.state_dict(), os.path.join(job_dir, f'manual_trained_model_{epoch}epoch.pth')) 
