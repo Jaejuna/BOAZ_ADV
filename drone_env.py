e2ccbd6359caf3ec6d502a07b36b872788011eae
이찬 <cksdlakstp12@naver.com>
2024-01-14 18:59:26 +0900

    ADD : gym based learning

---
+++ drone_env.py
@@ -0,0 +1,115 @@
+import gym
+from gym import spaces
+import airsim
+
+import time
+import numpy as np
+
+from tools.vision import *
+from tools.train_utils import * 
+
+class AirSimEnv(gym.Env):
+    metadata = {"render.modes": ["rgb_array"]}
+
+    def __init__(self, image_shape):
+        self.observation_space = spaces.Box(0, 255, shape=image_shape, dtype=np.uint8)
+        self.viewer = None
+
+    def __del__(self):
+        raise NotImplementedError()
+
+    def _get_obs(self):
+        raise NotImplementedError()
+
+    def _compute_reward(self):
+        raise NotImplementedError()
+
+    def close(self):
+        raise NotImplementedError()
+
+    def step(self, action):
+        raise NotImplementedError()
+
+    def render(self):
+        return self._get_obs()
+
+class AirSimDroneEnv(AirSimEnv):
+    def __init__(self, ip_address, step_length, image_shape, args):
+        super().__init__(image_shape)
+        self.step_length = step_length
+        self.image_shape = image_shape
+        self.args = args
+
+        self.state = {
+            "position": np.zeros(3),
+            "collision": False,
+            "prev_position": np.zeros(3),
+        }
+
+        self.drone = airsim.MultirotorClient(ip=ip_address)
+        self.action_space = spaces.Discrete(3)
+        self._setup_flight()
+        self.initial_z = self.drone.getMultirotorState().kinematics_estimated.position.z_val
+
+        self.image_request = airsim.ImageRequest(
+            3, airsim.ImageType.DepthPerspective, True, False
+        )
+
+        self.global_pcd = o3d.geometry.PointCloud()
+        self.map_pcd_len = len(getMapPointCloud(args).points)
+
+    def __del__(self):
+        self.drone.reset()
+
+    def _setup_flight(self):
+        self.drone = resetState(self.drone)
+        self.running_time = 0
+
+    def transform_obs(self, responses):
+        img1d = np.array(responses[0].image_data_float, dtype=np.float32)
+        img1d = 255 / np.maximum(np.ones(img1d.size), img1d)
+        img2d = np.reshape(img1d, (responses[0].height, responses[0].width))
+        img2d = np.expand_dims(img2d, axis=-1)
+        return img2d
+
+    def _get_obs(self):
+        responses = self.drone.simGetImages([self.image_request])
+        image = self.transform_obs(responses)
+        
+        self.curr_pcd = get_transformed_lidar_pc(self.drone)
+        global_pcd = self.global_pcd + self.curr_pcd    
+        self.global_pcd = global_pcd.voxel_down_sample(voxel_size=self.args.voxel_size)
+
+        self.drone_state = self.drone.getMultirotorState()
+
+        self.state["prev_position"] = self.state["position"]
+        self.state["position"] = self.drone_state.kinematics_estimated.position
+        self.state["velocity"] = self.drone_state.kinematics_estimated.linear_velocity
+        self.state["collision"] = self.drone.simGetCollisionInfo().has_collided
+
+        return image
+
+    def _do_action(self, action):            
+        if action == 0:
+            current_z = self.drone.getMultirotorState().kinematics_estimated.position.z_val
+            v = args["drone"].default_velocity
+            x, y, _ = calcForwardDirection(self.drone)
+            z = (self.initial_z - current_z) * 0.5 
+            move_with_timeout(self.drone, x * v, y * v, z, 1) 
+        else:
+            rotate_with_timeout(self.drone, calcDegree(self.drone, action), 1) 
+
+    def _compute_reward(self):
+        reward = calcReward(self.global_pcd, self.curr_pcd, self.args) 
+        done = len(self.global_pcd.points) >= self.map_pcd_len * 0.7
+        return reward, done
+
+    def step(self, action):
+        self._do_action(action)
+        obs = self._get_obs()
+        reward, done = self._compute_reward()
+        return obs, reward, done, self.state
+
+    def reset(self):
+        self._setup_flight()
+        return self._get_obs()
--- ray.py
+++ ray.py
@@ -1,181 +1,74 @@
-import ray
-from ray.rllib.algorithms.ppo import PPOTrainer, PPOConfig
-from ray import tune
-
+# import setup_path
 import gym
-from gym import spaces
+import airgym
 
-import airsim
+import os
+from datetime import datetime
+import time
 
-import numpy as np
-
-from tools.test import getAccuracy
-from tools.models import MovePredictModel
-from tools.vision import *
-from tools.train_utils import * 
-from tools.live_visualization import live_visualization
+from drone_env import AirSimDroneEnv
+from stable_baselines3 import DQN
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import VecTransposeImage
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback
 
 from config.default import args
 
-class CustomAirSim(gym.Env):
-    def __init__(self, config=None):
-        # AirSim 클라이언트와 기타 필요한 변수를 초기화
-        self.client = airsim.MultirotorClient()
-        self.client.confirmConnection()
-        self.client.enableApiControl(True)
-        self.client.armDisarm(True) # 드론의 시동을 걸어줌, 가상 드론의 모터를 활성화하고 비행 준비 상태로 전환
-        self.client.takeoffAsync().join() # 이륙, join()은 이륙 작업이 완료될 때까지 기다리는 역할
-        self.client.hoverAsync().join() # 현재위치에서 정지비행상태로 전환, join은 완료할 때까지 기다리는 역할
+date = datetime.now().strftime('%Y-%m-%d_%Hh%Mm%Ss')
+job_dir = os.path.join("./run", date)
+os.makedirs(job_dir, exist_ok=True)
 
-        # 행동 및 관찰 공간을 정의합니다.
-        self.action_space = spaces.Discrete(8)  # [앞,뒤,좌,우,위,아래,왼회전,오른회전]? '행동 세부 정의 필요'
-        self.observation_space = spaces.Box(low=0, high=255, shape=(...), dtype=np.uint8)  # '원하는 이미지 정의 필요'
-        
-        # ppo 알고리즘 초기화
-        self.init_ppo_config()
-        
-    def init_ppo_config(self):
-        self.ppo_config = PPOConfig()
-        # kl_coeff, ->
-        # vf_loss_coeff used to be 0.01??
-        # "entropy_coeff": 0.00005,
-        # "clip_param": 0.1,
-        self.ppo_config.gamma = 0.998  # default 0.99
-        self.ppo_config.lambda_ = 0.99  # default 1.0???
-        self.ppo_config.kl_target = 0.01  # used to use 0.02
-        self.ppo_config.rollout_fragment_length = 512
-        self.ppo_config.train_batch_size = 6400
-        self.ppo_config.sgd_minibatch_size = 256
-        self.ppo_config.num_sgd_iter = 2 # default 30???
-        self.ppo_config.lr = 3.5e-5  # 5e-5
-        self.ppo_config.model = {
-            # Share layers for value function. If you set this to True, it's
-            # important to tune vf_loss_coeff.
-            "vf_share_layers": False,
+# Create a DummyVecEnv for main airsim gym env
+env = AirSimDroneEnv(
+    ip_address="127.0.0.1",
+    step_length=0.25,
+    image_shape=(224, 224, 1),
+    args=args
+)
 
-            "use_lstm": True,
-            "max_seq_len": 32,
-            "lstm_cell_size": 128,
-            "lstm_use_prev_action": True,
+# Wrap env as VecTransposeImage to allow SB to handle frame observations
+# env = VecTransposeImage(env)
 
-            # 'use_attention': True,
-            # "max_seq_len": 128,
-            # "attention_num_transformer_units": 1,
-            # "attention_dim": 1024,
-            # "attention_memory_inference": 128,
-            # "attention_memory_training": 128,
-            # "attention_num_heads": 8,
-            # "attention_head_dim": 64,
-            # "attention_position_wise_mlp_dim": 512,
-            # "attention_use_n_prev_actions": 0,
-            # "attention_use_n_prev_rewards": 0,
-            # "attention_init_gru_gate_bias": 2.0,
+# Initialize RL algorithm type and parameters
+model = DQN(
+    "CnnPolicy",
+    env,
+    learning_rate=0.00025,
+    verbose=1,
+    batch_size=32,
+    train_freq=4,
+    target_update_interval=10000,
+    learning_starts=10000,
+    buffer_size=500000,
+    max_grad_norm=10,
+    exploration_fraction=0.1,
+    exploration_final_eps=0.01,
+    device="cuda",
+    tensorboard_log=job_dir,
+)
 
-            "conv_filters": [
-                # [4, [3, 4], [1, 1]],
-                # [16, [6, 8], [3, 3]],
-                # [32, [6, 8], [3, 4]],
-                # [64, [6, 6], 3],
-                # [256, [9, 9], 1],
+# Create an evaluation callback with the same env, called every 10000 iterations
+callbacks = []
+eval_callback = EvalCallback(
+    env,
+    callback_on_new_best=None,
+    n_eval_episodes=5,
+    best_model_save_path=job_dir,
+    log_path=job_dir,
+    eval_freq=10000,
+)
+callbacks.append(eval_callback)
 
-                # 480 x 640
-                # [4, [7, 7], [3, 3]],
-                # [16, [5, 5], [3, 3]],
-                # [32, [5, 5], [2, 2]],
-                # [64, [5, 5], [2, 2]],
-                # [256, [5, 5], [3, 5]],
+kwargs = {}
+kwargs["callback"] = callbacks
 
-                # 240 X 320
-                [16, [5, 5], 3],
-                [32, [5, 5], 3],
-                [64, [5, 5], 3],
-                [128, [3, 3], 2],
-                [256, [3, 3], 2],
-                [512, [3, 3], 2],
-            ],
-            "conv_activation": "relu",
-            "post_fcnet_hiddens": [512],
-            "post_fcnet_activation": "relu"
-        }
-        self.ppo_config.batch_mode = "complete_episodes"
-        self.ppo_config.simple_optimizer = True
-        self.ppo_config.num_gpus = 1
+# Train for a certain number of timesteps
+model.learn(
+    total_timesteps=5e5,
+    tb_log_name="dqn_airsim_drone_run_" + str(time.time()),
+    **kwargs
+)
 
-        self.ppo_config.rollouts(num_rollout_workers=0)
-
-        self.ppo_config.env = None
-        self.ppo_config.observation_space = spaces.Box(low=0, high=1, shape=('y','x', 1), dtype=np.float32) # 원하는 상태(이미지) 데이터 형태 정의 필요
-        self.ppo_config.action_space = spaces.MultiDiscrete(    # agent action 가짓수 정의 필요 
-            [
-                2,  # W
-                2,  # A
-                2,  # S
-                2,  # D
-                2,  # Space
-                2,  # H
-                2,  # J
-                2,  # K
-                2  # L
-            ]
-        )
-        self.ppo_config.env_config = {
-            "sleep": True,
-        }
-        self.ppo_config.framework_str = 'tf'
-        self.ppo_config.log_sys_usage = False
-        self.ppo_config.compress_observations = True
-        self.ppo_config.shuffle_sequences = False
-        
-        return self.ppo_config    
-    
-    # 필요한 경우 PPO 설정을 업데이트하는 메서드
-    #def update_ppo_config(self, new_config):
-    #    for key, value in new_config.items():
-    #        setattr(self.ppo_config, key, value)
-
-    def reset(self):
-        # 환경을 초기 상태로 리셋하고 초기 관찰값을 반환합니다.
-        self.client.reset()
-        return self._get_observation()
-
-    def step(self, action):
-        # 행동을 실행하고, 다음 상태, 보상, 종료 여부, 추가 정보를 반환합니다.
-        reward = 0
-        done = False
-        info = {}
-
-        # ... 행동 실행 ...
-
-        observation = self._get_observation()
-        return observation, reward, done, info
-
-    def _get_observation(self):
-        # 현재 상태 (예: RGB 이미지)를 반환하는 도우미 함수
-        responses = self.client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.Scene)])
-        response = responses[0]
-        img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8)
-        img_rgb = img1d.reshape(response.height, response.width, 3)
-        
-        return img_rgb
-
-    def close(self):
-        # 환경을 종료하기 전에 호출됩니다.
-        self.client.enableApiControl(False)
-        
-if __name__ == "__main__":
-    
-    trainer = PPOTrainer(env=CustomAirSim, config={
-        "env_config": {},
-        # 다른 설정 ...
-    })
-
-    ray.init()
-    # ray.init(num_cpus=4, num_gpus=1, log_to_driver=False) #init example
-    trainer = PPOTrainer
-    env = CustomAirSim()
-    
-    tune.run(trainer,
-            resume='AUTO',
-            config=env.ppo_config.to_dict(), keep_checkpoints_num=None, checkpoint_score_attr="episode_reward_mean",
-            max_failures=1,
-            checkpoint_freq=5, checkpoint_at_end=True)
+# Save policy weights
+model.save("dqn_airsim_drone_policy")
